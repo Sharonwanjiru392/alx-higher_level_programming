8d909dcbed7120fae1d7c096612b4ed9505912ea
Sharon <Sharonwanjiru392@gmail.com>
2023-06-05 04:57:46 -0700

    6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "school"
-str3 += " " + str2
-print("welcome to {}!".format))
+str2 = "School"
+str1 += " " + str2
+print("welcome to {}!".format(str1))
