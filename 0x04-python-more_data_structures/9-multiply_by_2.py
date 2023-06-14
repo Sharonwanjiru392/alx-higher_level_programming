874ecaace0dc01e86bb01d4b65ac92978bc7ca6f
Sharon <Sharonwanjiru392@gmail.com>
2023-06-14 15:37:09 -0700

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multipy_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_diir.keys())
+
+    for i in list_keys:
+        new_dir[i] *= 2
+
+    return (new_dir)
