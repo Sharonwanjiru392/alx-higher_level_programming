1599de8d6430056677cc301e513f9c5b8b1177a3
Sharon <Sharonwanjiru392@gmail.com>
2023-07-08 04:00:01 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,7 @@
-#include "hash_tables.h"
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
 
-/**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
- */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *ht;
-	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-	return (ht);
-}
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
