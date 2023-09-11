f7aa196ab71bf7903a04e13483819ad467011089
Sharon <Sharonwanjiru392@gmail.com>
2023-09-11 13:16:58 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 /* will print 3 line using console */
-console.log('C'is'fun');
-console.log('Python'is'cool');
-console.log('JavaScript'is'amizing');
+console.log('C' is 'fun');
+console.log('Python' is 'cool');
+console.log('JavaScript' is 'amizing');
