72ab70e364a4a243690b2a0ff3e6253ca35485ba
Sharon <Sharonwanjiru392@gmail.com>
2023-09-11 12:41:11 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstLine = 'c is fun';
+const secondLine = 'python is cool';
+const thirdLine = 'JavaScript is amaizing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
