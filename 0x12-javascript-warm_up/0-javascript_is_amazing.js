a9fb7c4a3107a5f55f52a66169dd2f65d3295989
Sharon <Sharonwanjiru392@gmail.com>
2023-09-11 12:57:41 -0700

    :0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-// print string "Javascript is amizing"
-const myvar = 'Javascript is amizing';
-console.log(myvar);
+// Print string “Javascript is amazing”
+
+const myVar = 'Javascript is amazing';
+console.log(myVar);
