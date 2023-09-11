a6a61f7064887617f670b66d74bd13416cd01db1
Sharon <Sharonwanjiru392@gmail.com>
2023-09-11 12:53:17 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argsLength = process.argv.length;
+if (argsLEngth === 2) {
+	console.log('NO argument');
+} else if (argsLength === 3) {
+	conssole.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
