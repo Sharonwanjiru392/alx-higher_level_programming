590dfac5dcc9090ba0398ba4035d00d9b3bd4c5a
Sharon <Sharonwanjiru392@gmail.com>
2023-09-28 14:29:29 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-https://www.google.com/search?q=who+is+sharon&oq=who+&gs_lcrp=EgZjaHJvbWUqCwgAEEUYJxg7GIoFMgsIABBFGCcYOxiKBTIGCAEQRRg5MgkIAhAAGEMYigUyCQgDEAAYQxiKBTIJCAQQABhDGIoFMgkIBRAAGEMYigUyCggGEAAYsQMYgAQyBwgHEC4YgAQyBggIEAAYAzINCAkQABixAxjJAxiABNIBCDQzMjZqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8
 curl -sI $1 | grep "Content-Length" | cut -d " " -f2
