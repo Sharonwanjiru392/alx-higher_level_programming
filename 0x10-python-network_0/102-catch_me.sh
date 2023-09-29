8875863a92cc16aa079c677e8face90b044c69bf
Sharon <Sharonwanjiru392@gmail.com>
2023-09-29 02:44:29 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#script thatrequest to 0.0.0.0:5000/catch_me that causes the server to respond
+#script that request to 0.0.0.0:5000/catch_me that causes the server to respond
 curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
